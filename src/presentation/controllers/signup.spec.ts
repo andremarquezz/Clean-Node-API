3541bd2d7ebeeeb7183a62362e045c506ee81c23
jey <andre.marqquez@gmail.com>
2022-12-05 07:23:42 -0300

    test: garante que SignupController retorna erro
    garante que SignupController retorna um erro quando o nome não é provido

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -13,5 +13,6 @@ describe('SignUp Controller', () => {
 
     const response = sut.handle(httpRequest)
     expect(response.status).toBe(400)
+    expect(response.body).toEqual(new Error("Missing param: 'name'"))
   })
 })
