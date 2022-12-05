74176eecffcedbf3161d4b0b772bb25e32379d79
jey <andre.marqquez@gmail.com>
2022-12-05 07:11:32 -0300

    test: adiciona SignupController e retorna 400

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,17 @@
+import { SignUpController } from './signup'
+
+describe('SignUp Controller', () => {
+  test('Shoul return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'jey@mail.com',
+        password: 'password',
+        passwordConfirmation: 'password'
+      }
+    }
+
+    const response = sut.handle(httpRequest)
+    expect(response.status).toBe(400)
+  })
+})
